b5804c3481278b498065b9f918136ae79c075ff3
IzzyNety <izzy.nety@gmail.com>
2020-02-11 11:38:40 +0500

    index + search
    little refactoring card-body

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -19,8 +19,8 @@
 
     <ul class="col-6 menu__box">
 
-        <form class="form-check-label my-0 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Найти пост..." aria-label="Search">
+        <form class="form-check-label my-0 my-lg-0" action="{{ route('post.index') }}">
+            <input class="form-control mr-sm-2" name="search" type="search" placeholder="Найти пост..." aria-label="Search">
         </form>
         <li class="nav-item active">
             <a class="menu__item" href="/">Главная</a>
@@ -34,16 +34,35 @@
 
 
     <div class="container">
+
+        @if(isset($_GET['search']))
+            @if(count($posts)>0)
+                <h2>Результаты поиска по запросу <?=$_GET['search']?></h2>
+                <p class="lead">Всего найдено {{ count($posts) }} постов</p>
+            @else
+                <h2>По запросу <?=$_GET['search']?> ничего не найдено</h2>
+                <a href="{{ route('post.index') }}" class="btn btn-outline-primary">Отобразить все посты</a>
+            @endif
+        @endif
+
         <div class="row">
             @foreach($posts as $post)
             <div class="col-6">
                 <div class="card">
-                    <div class="card-header">{{ $post->short_title }}</div>
-                    <div class="card-body">{{ $post->descr }}</div>
+                    <div class="card-header"><h2>{{ $post->short_title }}</h2></div>
+                    <div class="card-body">
+                        <div class="card-author"> Автор : {{ $post->name }}</div>
+                        <div class="card-body">{{ $post->descr }}</div>
+                        <a href="#" class="btn btn-outline-primary">Посмотреть пост</a>
+                    </div>
                 </div>
             </div>
             @endforeach
         </div>
+
+        @if(!isset($_GET['search']))
+        {{ $posts->links() }}
+        @endif
     </div>
 </body>
 </html>
